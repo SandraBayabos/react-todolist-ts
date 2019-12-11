06f4a2df592c5e8be3faf551ccb4fc8cc0e582b0
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-12-11 18:27:23 +0800

    created interfaces.ts

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,11 @@
+// Import dependencies
+import * as React from "react";
+
+// Import interfaces
+import { TodoItemInterface } from "./../interfaces";
+
+const TodoItem = (props: TodoItemInterface) => {
+  return <div className="todo-item"></div>;
+};
+
+export default TodoItem;
