37b3ff891483b3715a5a5acfba1613e573328a91
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-12-12 10:32:26 +0800

    created template for TodoItem component

---
+++ src/components/TodoForm.tsx
@@ -0,0 +1,11 @@
+// Import dependencies
+import * as React from "react";
+
+// Import interfaces
+import { TodoFormInterface } from "./../interfaces";
+
+const TodoForm = (props: TodoFormInterface) => {
+  return <div className="todo-form"></div>;
+};
+
+export default TodoForm;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,7 +5,33 @@ import * as React from "react";
 import { TodoItemInterface } from "./../interfaces";
 
 const TodoItem = (props: TodoItemInterface) => {
-  return <div className="todo-item"></div>;
+  return (
+    <div className="todo-item">
+      <div onClick={() => props.handleTodoComplete(props.todo.id)}>
+        {props.todo.isCompleted ? (
+          <span className="todo-item-checked">✔</span>
+        ) : (
+          <span className="todo-item-unchecked"></span>
+        )}
+      </div>
+      <div className="todo-item-input-wrapper">
+        <input
+          value={props.todo.text}
+          onBlur={props.handleTodoBlur}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            props.handleTodoUpdate(event, props.todo.id)
+          }
+        />
+      </div>
+
+      <div
+        className="item-remove"
+        onClick={() => props.handleTodoRemove(props.todo.id)}
+      >
+        x
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
